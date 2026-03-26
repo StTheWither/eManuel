e99b6e71971d9e932078f8a05f1025e2ed33aaee
StTheWither <stefanlenkov11@gmail.com>
2026-03-26 21:30:46 +0200

    frontend URL connection attempt 1

--- hacktues12/my-hacktues-app/src/App.jsx
+++ hacktues12/my-hacktues-app/src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+function MyList() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = "https://educrave.onrender.com/api/items";
+
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        setItems(data); 
+      })
+      .catch(error => console.error("Error connecting to backend:", error));
+  }, []);
+
+  return (
+    <ul>
+      {items.map(item => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0)
+  const apiUrl = process.env.REACT_APP_API_URL + "/api/items"; 
 
 
   function loginPage(){
