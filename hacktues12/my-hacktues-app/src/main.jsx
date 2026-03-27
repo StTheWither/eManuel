07c113874a8468a648be9bdd61e909e532ea9fa5
Ivanzhelev0 <ivan0zhelev@gmail.com>
2026-03-27 11:54:50 +0200

    try to start website

--- hacktues12/my-hacktues-app/src/main.jsx
+++ hacktues12/my-hacktues-app/src/main.jsx
@@ -3,9 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
-import AuthPage from './log_in_page.jsx'
-import SubscriptionApp from './subscription_page.jsx'
-import subjectAPP from './subject_page.jsx'
+
 
 
 
